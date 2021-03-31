f4246e5eb741599a2a4ce02dd00404e14d2f8e9e
Karen Carnaval <motok@MotokBook.localdomain>
2021-03-30 23:38:08 -0400

    03-product.js completed

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,8 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
-
+function product( num1 = 0, num2 = 1) {
+  return num1 * num2;
 }
 
 // ==================================================
@@ -20,21 +20,30 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
+  var result = product(2, 0);
+  if (result !== 0) throw new Error('Expected product(2, 0) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = product(2, 2, 2, 2, 2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = product(4);
+  if (result !== 4) throw new Error('Expected product(4) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = product();
+  if (result !== 0) throw new Error('Expected product() to be 0. Received: ' + result);
+
 
   console.log('All tests passed successfully.');
 
