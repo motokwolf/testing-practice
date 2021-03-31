44f930b4f986a5295fee196d820aae40aa652f03
Karen Carnaval <motok@MotokBook.localdomain>
2021-03-30 22:53:02 -0400

    Follow up class

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,7 +1,7 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum(num1, num2) {
+function sum(num1, num2 = 0) {
 	return num1 + num2;
 }
 
@@ -20,11 +20,15 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = sum(1, 1, 1, 1, 1, 1);
+  if (result !== 2) throw new Error('Expected sum(1, 1, 1, 1, 1, 1) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result =sum(2);
+  if (result !== 2) throw new Error('Expected sum(2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
