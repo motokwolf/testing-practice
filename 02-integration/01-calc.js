58876ff68b2c1ca6d365b37ab44a9d49c3942dd5
Karen Carnaval <motok@MotokBook.localdomain>
2021-03-30 23:57:07 -0400

    01-calc.js work

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -2,7 +2,8 @@
 // DEFINE FUNCTION(S)
 // ==================================================
 function calc(command, num1, num2) {
-   function sum(num1, num2) {
+  
+   function sum(num1 = 0, num2 = 0) {
     return num1 + num2;
    }
 
